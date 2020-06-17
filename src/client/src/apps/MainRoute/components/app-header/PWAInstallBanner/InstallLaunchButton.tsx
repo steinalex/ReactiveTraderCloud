6d275d818b7237a16c11f1ea97ff8dd84e1bdcec
Alex Stein <alex.stein@weareadaptive.com>
2020-06-17 16:19:56 +0100

    feat(workspace): fix install button bug (ARTP-1195)

--- src/client/src/apps/MainRoute/components/app-header/PWAInstallBanner/InstallLaunchButton.tsx
+++ src/client/src/apps/MainRoute/components/app-header/PWAInstallBanner/InstallLaunchButton.tsx
@@ -1,27 +1,22 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 import { PWABanner, InstallButton } from './PWAInstallBanner'
 import { usePWABannerPrompt } from './usePWABannerPrompt'
-
 interface InstallLaunchProps {
   bannerState: string | null
 }
-
 export const InstallLaunchButton: React.FC<InstallLaunchProps> = ({ bannerState }) => {
   const [prompt, setPrompt] = usePWABannerPrompt()
-  const hideAfterInstall = useRef<boolean>(false)
+  const [hideAfterInstall, setHideAfterInstall] = useState<boolean>(false)
 
   window.addEventListener('appinstalled', evt => {
-    console.log('APP JUST INSTALLED, HIDE INSTALL BUTTON')
-    hideAfterInstall.current = true
+    setHideAfterInstall(true)
   })
 
-  console.log(hideAfterInstall)
+  const isHidden = bannerState !== PWABanner.Shown
 
-  const isHidden = hideAfterInstall.current || bannerState !== PWABanner.Shown
-
-  if (prompt !== null && isHidden) {
-    return <InstallButton onClick={setPrompt}>Install PWA</InstallButton>
-  } else {
-    return <></>
+  if (hideAfterInstall || prompt === null || !isHidden) {
+    return null
   }
+
+  return <InstallButton onClick={setPrompt}>Install PWA</InstallButton>
 }
